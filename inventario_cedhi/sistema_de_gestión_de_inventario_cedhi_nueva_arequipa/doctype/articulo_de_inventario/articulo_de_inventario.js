3bb05383d0e81f9f597161689c027c11cd3413e0
Anthony Briceño <anthonyquiroz305@gmail.com>
2026-06-21 18:35:53 -0500

    Test

---
+++ inventario_cedhi/sistema_de_gestión_de_inventario_cedhi_nueva_arequipa/doctype/articulo_de_inventario/articulo_de_inventario.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, CEDHI Nueva Arequipa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Articulo de Inventario", {
+// 	refresh(frm) {
+
+// 	},
+// });
